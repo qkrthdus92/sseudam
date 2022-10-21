af982bbbed3c84c9b6febc326c8e864245634f57
DESKTOP-FKC274L\qkrth <qkrthdus0507@naver.com>
2022-10-21 09:19:02 +0900

    더미파일 추가 마이페이지 서비스다오 작업중

--- workspace/sseudamPrj/src/com/kh/sseudam/mypage/controller/ProMypageEditController.java
+++ workspace/sseudamPrj/src/com/kh/sseudam/mypage/controller/ProMypageEditController.java
@@ -14,13 +14,13 @@ public class ProMypageEditController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		HttpSession s = req.getSession();
-//		MemberVo loginMember = (MemberVo)s.getAttribute("loginMember");
-//		if(loginMember != null) {
-//			req.getRequestDispatcher("/views/mypage/proeditmypage.jsp").forward(req, resp);
-//		}else {
-//			//에러페이지
-//		}
+		HttpSession s = req.getSession();
+		MemberVo loginMember = (MemberVo)s.getAttribute("loginMember");
+		if(loginMember != null) {
+			req.getRequestDispatcher("/views/mypage/proeditmypage.jsp").forward(req, resp);
+		}else {
+			//에러페이지
+		}
 	}
 	
 	@Override
